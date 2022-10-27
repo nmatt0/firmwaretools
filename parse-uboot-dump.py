cb9f4d41a974039369958f692dd88f28375357f6
Matt Brown <matt@nmatt.com>
2022-10-27 00:22:56 -0400

    add uboot memdump parser

---
+++ parse-uboot-dump.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+i = open(infile,"r")
+o = open(outfile,"wb")
+
+for line in i.readlines():
+    line = line.strip()
+    if re.match(r'^[0-9a-f]{8}:',line):
+        line = line.split(":")
+        if len(line) == 2:
+            line = line[1]
+            line = line.replace(" ","")[:32]
+            data = bytes.fromhex(line)
+            o.write(data)
