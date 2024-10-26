b895ca1b0c1ec2eb2021b52cf64864414df26552
Jens Heine <binbash@gmx.net>
2024-10-26 18:38:13 +0200

    Tolerate 0x prefixes before hex data

--- parse-uboot-dump.py
+++ parse-uboot-dump.py
@@ -10,10 +10,12 @@ o = open(outfile,"wb")
 
 for line in i.readlines():
     line = line.strip()
-    if re.match(r'^[0-9a-f]{8}:',line):
+    if re.match(r'^[0-9a-f]{8}:',line) or re.match(r'^0x[0-9a-f]{8}:',line):
         line = line.split(":")
+        print(line)
         if len(line) == 2:
             line = line[1]
+            line = line.replace("0x","")
             line = line.replace(" ","")[:32]
             data = bytes.fromhex(line)
             o.write(data)
